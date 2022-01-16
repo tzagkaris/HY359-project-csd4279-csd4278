1942862b9695a06d769c661a310d1e843a689dbf
tzagkaris <csd4279@csd.uoc.gr>
2022-01-17 01:21:41 +0200

    register page show complete

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -9,7 +9,12 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
+  isDoctor: boolean = false;
+
   ngOnInit(): void {
   }
 
+  toggleMoreRegOptions= () => {
+    this.isDoctor = !this.isDoctor;
+  }
 }
